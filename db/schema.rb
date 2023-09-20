d28240d4c850f9895ccd6c9c7ce3f7fed6dc2954
Rails udemy course <railsudemycourse@gmail.com>
2023-09-20 14:26:30 +0500

    create article table,model and add validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_115853) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_091827) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
 end
