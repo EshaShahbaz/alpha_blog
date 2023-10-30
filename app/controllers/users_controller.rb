881209d0ff70b1459bbeed897d3411d53c6b2b0c
Rails udemy course <railsudemycourse@gmail.com>
2023-10-30 15:43:47 +0500

    Add edit update functonality and partials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,31 @@ class UsersController < ApplicationController
         render 'new'
         end 
        end  
+       def edit
+
+        @user = User.find(params[:id])
+        
+        end
+        def update
+
+            @user = User.find(params[:id])
+            
+            if @user.update(user_params)
+            
+            flash[:success] = "Your account was updated successfully"
+            
+            redirect_to articles_path
+            
+            else
+            
+            render 'edit'
+            
+            end
+            
+            end
         private
         def user_params
         params.require(:user).permit(:username, :email, :password)
         end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   get 'pages/about'
   #root 'pages#about'
   get 'signup', to: 'users#new'
-  post 'users', to: 'users#create'
+  resources :users, except: [:new]
+
 
 # namespace :admin do
 #   resources :pages
