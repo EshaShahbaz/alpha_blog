0a77a806d0a8704284bc4a9a608965c9d2cb1fb9
Rails udemy course <railsudemycourse@gmail.com>
2023-11-03 09:56:17 +0500

    update current user to article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ end
 def create
   #debugger
   @article = Article.new(article_params)
-  @article.user = User.first
+  @article.user = current_user
   if @article.save
   
   flash[:success] = "Article was successfully created"
