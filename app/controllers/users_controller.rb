3b7ef0d7bc492118fdb070a460707dc60d914521
Rails udemy course <railsudemycourse@gmail.com>
2023-11-03 10:42:58 +0500

    Add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ end
 def create
     @user = User.new(user_params)
     if @user.save
+    session[:user_id]= @user.id
     flash[:success] = "Welcome to the alpha blog #{@user.username}"
-    redirect_to articles_path
+    redirect_to user_path(@user)
     else
     render 'new'
     end 
@@ -46,13 +47,9 @@ def user_params
 end
 
 def require_same_user
-
     if current_user != @user
-    
     flash[:danger] = "You can only edit your own account"
-    
-    redirect_to root_path
-    
+    redirect_to root_path  
     end
 end
 
